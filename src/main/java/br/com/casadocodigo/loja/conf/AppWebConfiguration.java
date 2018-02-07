d432fdc777675268f3b07908ccf70f425b3aeabe
Thiago Garcia <thiagoufg@gmail.com>
2018-02-07 21:07:58 -0200

    Aula 2 Video 2 ViewResolver e ContentNegotiation

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,10 +1,10 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -14,10 +14,13 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -75,5 +78,16 @@ public class AppWebConfiguration {
 		GuavaCacheManager gcm = new GuavaCacheManager();
 		gcm.setCacheBuilder(builder);
 		return gcm;
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager man) {
+		ArrayList<ViewResolver> lista = new ArrayList<ViewResolver>();
+		lista.add(internalResourceViewResolve());
+		lista.add(new JsonViewResolver());
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(lista);
+		resolver.setContentNegotiationManager(man);
+		return resolver;
 	}
 }
